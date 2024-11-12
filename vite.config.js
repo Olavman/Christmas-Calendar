6730b378910f83c77bd39c49b93792f2c7c64429
Olavman <159426814+Olavman@users.noreply.github.com>
2024-11-13 00:25:44 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-    base: '/Christmas-Calendar/',
+  base: '/Christmas-Calendar/', // This should match the name of your GitHub repository
   plugins: [react()],
-})
+});
